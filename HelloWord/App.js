32cee834fd60b4d1fce3a87250e1c3be39f68b26
nuhaalarfaj1 <33942825+nuhaalarfaj1@users.noreply.github.com>
2023-10-09 18:33:44 +0000

    PE01 - Nuha Alarfaj

---
+++ HelloWord/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Nuha Alarfaj</Text>
+      <Text>CS624</Text>
+      <Text>Student</Text>
+    
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFF00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
